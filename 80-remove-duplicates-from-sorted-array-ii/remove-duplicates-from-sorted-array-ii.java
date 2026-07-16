80d761c741e08f314299bd02f2338a6ec93ed97f
Yadnesh Matale <25mataleyg@rbunagpur.in>
2026-07-16 17:35:06 +0530

    Time: 0 ms (100.00%) | Memory: 48.8 MB (32.28%) - LeetSync

---
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+         int n = nums.length;
+         int i = 2;
+         int j = 2;
+
+         while(j<n){
+            if(nums[j]!=nums[i-2]){
+                nums[i] = nums[j];
+                i++;
+            }
+            j++;
+         }
+         return i;
+    }
+}
